a9f3132aed44c9c2e4bf7f54246043185d7b4a6a
Ray <rayfradella@yahoo.com>
2026-03-04 01:14:33 -0500

    fix: standardize spacing and formatting in HTML and JavaScript files

--- src/app.js
+++ src/app.js
@@ -3,22 +3,22 @@ import React from 'react'
 function App(){
     const completeStyle =
     {
-        display: "flex",
-        justifyContent: "space-evenly",
-        backgroundColor: "lightgreen",
-        border: "2px dashed black",
-        padding: "5px",
-        margin: "5px"
+        display : "flex",
+        justifyContent : "space-evenly",
+        backgroundColor : "lightgreen",
+        border : "2px dashed black",
+        padding : "5px",
+        margin : "5px"
     }
 
     const incompleteStyle =
     {
-        display: "flex",
-        justifyContent: "space-evenly",
-        backgroundColor: "white",
-        border: "2px solid black",
-        padding: "10px",
-        margin: "10px"
+        display : "flex",
+        justifyContent : "space-evenly",
+        backgroundColor : "white",
+        border : "2px solid black",
+        padding : "10px",
+        margin : "10px"
     }
 
     var [todoList, setTodoList] = React.useState([])
@@ -41,7 +41,7 @@ function App(){
             )
         )
     }
-
+    {/*maybe combine this with addTodoDisplay?*/}
     const addTodo = ({todoName}) =>
     {
         if(todoName === ""){
@@ -51,10 +51,10 @@ function App(){
             prevTodos => [
                 ...prevTodos,
                 {
-                    id: Math.random(),
-                    date: new Date().toString(),
-                    completed: false,
-                    todoText: todoName
+                    id : Math.random(),
+                    date : new Date().toString(),
+                    completed : false,
+                    todoText : todoName
                 }
             ]
         )
@@ -82,24 +82,24 @@ function App(){
         todo => {
             return (
                 <div
-                    key={todo.id}
-                    style={
+                    key = {todo.id}
+                    style = {
                         todo.completed ?
                             completeStyle : incompleteStyle
                     }
                 >
                     <input
-                        name="todoCheckbox"
-                        type="checkbox"
-                        placeholder="Toggle a todo item"
-                        onChange={
+                        name = "todoCheckbox"
+                        type = "checkbox"
+                        placeholder = "Toggle a todo item"
+                        onChange = {
                             () => markComplete(todo.id)
                         }
                     />
                     <p>{todo.todoText}</p>
                     {/*consider better names for arrow variables*/}
                     <button
-                        onClick={
+                        onClick = {
                             () => setTodoList(
                                 prevTodos => prevTodos.filter(
                                     currTodo => currTodo.id !== todo.id
